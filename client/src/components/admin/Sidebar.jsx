2e5d17642b4225a0f770dd4fa96243f27210db32
irshad12332123 <alidanish5138@gmail.com>
2025-11-19 21:44:35 +0530

    moved log out button to sidebar to main consistency

--- client/src/components/admin/Sidebar.jsx
+++ client/src/components/admin/Sidebar.jsx
@@ -4,8 +4,17 @@ import { BsCalendar2Event } from "react-icons/bs";
 import { LuUserRound } from "react-icons/lu";
 import { SiCoursera } from "react-icons/si";
 import { NavLink } from "react-router-dom";
+import CustomBtn from "../CustomBtn";
+import { useApiContext } from "@/providers/ApiContext";
 const Sidebar = () => {
   const { username_id } = decodeToken();
+  const { handleLogOut } = useApiContext();
+
+  const logOut = () => {
+    const hasLoggedOut = handleLogOut();
+    if (hasLoggedOut) navigate("/admin-login");
+  };
+
   const tabs = [
     { Icon: BsCalendar2Event, title: "Events Management", to: "/admin-event" },
     { Icon: SiCoursera, title: "Courses Management", to: "/admin-course" },
@@ -17,13 +26,20 @@ const Sidebar = () => {
           `}
     >
       <div className="flex items-center justify-between mb-8">
-        <h2 className="text-xl font-semibold text-white">
-          Next <span className="text-celestial-blue ">Gen</span>
-        </h2>
+        <div className="flex gap-2 items-center ">
+          <LuUserRound
+            className="rounded-full p-2 bg-[#AFC0A9] text-raisin-black"
+            size={40}
+          />
+          <div className="">
+            <p className="text-wheat">{username_id}</p>
+            <p className="text-muted text-sm">logged in</p>
+          </div>
+        </div>
       </div>
 
       {/* Navigation */}
-      <nav className="flex flex-col gap-1 flex-grow">
+      <nav className="flex flex-col gap-1 flex-grow border-t pt-2 border-[#dadada4f]">
         <nav className="flex flex-col gap-1 flex-grow">
           {tabs.map((tab, i) => (
             <NavLink
@@ -32,7 +48,7 @@ const Sidebar = () => {
               className={({ isActive }) =>
                 `
         flex items-center gap-2 py-2 px-4 rounded-md cursor-pointer transition-all
-        ${isActive ? "bg-celestial-blue text-white" : "bg-[#25384953]"}
+        ${isActive ? "bg-celestial-blue text-white" : "bg-[#25384900]"}
         hover:opacity-70
         `
               }
@@ -48,15 +64,9 @@ const Sidebar = () => {
         </nav>
 
         <div className="flex-grow"></div>
-        <div className="flex gap-2 items-center">
-          <LuUserRound
-            className="rounded-full p-2 bg-[#AFC0A9] text-raisin-black"
-            size={40}
-          />
-          <div className="">
-            <p className="text-wheat">{username_id}</p>
-            <p className="text-muted text-sm">logged in</p>
-          </div>
+
+        <div>
+          <CustomBtn title={"Log out"} onClick={logOut} />
         </div>
       </nav>
     </aside>
